7cb0df07f6367707ca1c30c96eb7d2eec04238ff
Matt Miller <mattmiller0989@gmail.com>
2020-04-16 03:17:40 -0500

    I got the compare_code to work properly

--- Mastermind.rb
+++ Mastermind.rb
@@ -45,29 +45,50 @@ class Game_runner
     
 end
 class Code
-    @@pegs=[]
+    #@@pegs=[]
     def initialize(a0,a1,a2,a3)
         # @a0=a0
         # @a1=a1
         # @a2=a2
         # @a3=a3
-        @@pegs=[a0,a1,a2,a3]
+        @pegs=[a0,a1,a2,a3]
     end
     def compare_code(other_code)
         
+        puts other_code.get_pegs
+        helper_pegs=[]
+        other_pegs=other_code.get_pegs.clone
+        
+        @pegs.each_with_index do |element,index|
+            if element == other_pegs[index]
+                puts "element: "+element.to_s+" index: "+index.to_s+" other_pegs[index]: "+other_pegs[index].to_s
+                helper_pegs.push("r")
+                other_pegs[index]=" "
+            end
+        end
+        
+        other_pegs.each_with_index do |e,ind|
+            if @pegs.include?(e)
+                helper_pegs.push("w")
+                other_pegs[ind]=" "
+            end
+        end
+        
+        return helper_pegs
+
     end
     def get_pegs
-        return @@pegs
+        return @pegs
     end
 end
 
 class Game_code < Code
 
-    @@pegs=[]
+    @pegs=[]
     def initialize()
         
         (0...4).each do |n|
-            @@pegs[n]=["R","G","B","P","Y","W"].sample
+            @pegs[n]=["R","G","B","P","Y","W"].sample
         end
     end
 
@@ -86,13 +107,16 @@ end
 
 #my_runner=Game_runner.new
 my_board=Game_board.new
-my_row=Game_row.new("R","G","B","Y")
+my_row=Game_row.new("R","P","B","W")
 my_board.put_row(1,my_row)
 
-my_new_row=Game_row.new("B","P","G","W")
+my_new_row=Game_row.new("P","P","Y","Y")
 my_board.put_row(2,my_new_row)
 
+helper=my_new_row.compare_code(my_row)
+helper_code=Code.new(helper[0],helper[1],helper[2],helper[3])
 
-master_code=Game_code.new
-my_board.put_row(4,master_code)
-my_board.print_board
+my_board.put_row(6,helper_code)
+# master_code=Game_code.new
+# my_board.put_row(4,master_code)
+ my_board.print_board
